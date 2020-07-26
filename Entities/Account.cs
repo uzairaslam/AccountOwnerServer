7e21e28a4f5386b4ee4beb583ddf0ec36454403e
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2020-07-26 23:29:16 +0500

    Creating Models

---
+++ Entities/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities
+{
+    [Table("account")]
+    public class Account
+    {
+        public Guid AccountId { get; set; }
+
+        [Required(ErrorMessage = "Date created is required")]
+        public DateTime DateCreated { get; set; }
+
+        [Required(ErrorMessage = "Account type is required")]
+        public string AccountType { get; set; }
+
+        [ForeignKey(nameof(Owner))]
+        public Guid OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
