145bf91ca5007cb508db687af57b16f16be7d7be
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2020-07-28 20:00:00 -0600

    Repository Pattern Logic

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Repository
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext _repositoryContext { get; set; }
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public void Create(T entity)
+        {
+            this._repositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this._repositoryContext.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return this._repositoryContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this._repositoryContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Update(T entity)
+        {
+            this._repositoryContext.Set<T>().Update(entity);
+        }
+    }
+}
