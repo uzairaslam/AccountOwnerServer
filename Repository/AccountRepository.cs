a5c49b52d5a7308e2961e0be4219edcddabf48c3
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2020-07-29 20:00:00 -0600

    Repository User Classes

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class AccountRepository : RepositoryBase<Account>, IAccountRepository
+    {
+        public AccountRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
